b3f6b3e8e0103195c914ed72ee204641cc9ddaaa
wangyao <wangyao_19941@hotmail.com>
2018-05-02 14:54:03 +0900

    default

---
+++ psnr.py
@@ -0,0 +1,38 @@
+"""
+Video Quality Metrics
+Copyright (c) 2014 Alex Izvorski <aizvorski@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import numpy
+import math
+
+def psnr(img1, img2):
+    mse = numpy.mean( (img1 - img2) ** 2 )
+    if mse == 0:
+        return 100
+    PIXEL_MAX = 255.0
+    return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
+
+import scipy.misc
+
+ref_file = 'compare/2-output.png'
+dist_file = 'compare/2-targets.png'
+
+ref = scipy.misc.imread(ref_file, flatten=True).astype(numpy.float32)
+dist = scipy.misc.imread(dist_file, flatten=True).astype(numpy.float32)
+
+psnr_value = psnr(ref, dist)
+print(psnr_value)
